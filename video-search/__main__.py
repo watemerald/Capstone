e9136e8c920f946badfd4db66f00c0c16cebeb12
Andrii Sherman <andriisherman@gmail.com>
2020-06-30 17:16:12 -0400

    Basic parameter passing

--- video-search/__main__.py
+++ video-search/__main__.py
@@ -1,16 +1,41 @@
 import typer
+from typer import Argument, Option
+from pathlib import Path
 
-from .simple_model import predict
+from .simple_model import predict, train
+from .utils import create_logger
 
 app = typer.Typer()
 
+log = create_logger(__name__, "file.log")
 
 # Hyperparameters for the models
+FOLDER = "/media/watemerald/Seagate/data/yt8m/video/"
+
+BATCH_SIZE = 10 * 1024
+
+# Number of epochs
+N_EPOCHS = 100
+
+# Save the weights every N iterations
+N_ITR = 10
 
 
-@app.command()
-def train():
-    print("Let's get training")
+@app.command("train")
+def train_model(
+    media_folder: Path = Option(
+        FOLDER, help="The folder where the YouTube-8M files are stored"
+    ),
+    batch: int = Option(BATCH_SIZE, help="Number of records to process per batch"),
+    epochs: int = Option(N_EPOCHS, help="Total number of epochs to train for"),
+    save_interval: int = Option(N_ITR, help="How often to save intermediate results"),
+):
+    typer.echo(f"media_folder: {media_folder}")
+    typer.echo(f"batch: {batch}")
+    typer.echo(f"epochs: {epochs}")
+    typer.echo(f"save_interval: {save_interval}")
+
+    typer.echo(f"locals: {locals()}")
 
 
 @app.command("predict")
